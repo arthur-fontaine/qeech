8715d0bf491c6d852ffa76b6ed5efe0a942f2907
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-26 17:16:14 +0200

    First implementation of season_score

--- data/qeech_data/application/services/season_score.py
+++ data/qeech_data/application/services/season_score.py
@@ -1,3 +1,11 @@
-def season_score():
-    # TODO: implement
-    pass
+from datetime import datetime
+
+from qeech_data.core.entities.recipe import Recipe
+
+def season_score(recipe: Recipe, date: datetime):
+    average_recipe_date = recipe.get_average_interaction_period()
+
+    days_since_start_of_year = (date - datetime(date.year, 1, 1)).days
+
+    # More the dates are close to each other, more the score is close to 1
+    return 1 - abs(days_since_start_of_year - average_recipe_date) / 365
--- data/qeech_data/core/entities/recipe.py
+++ data/qeech_data/core/entities/recipe.py
@@ -1,4 +1,39 @@
+from datetime import datetime
+
 from qeech_data.core.entities.ingredient import Ingredient
+from qeech_data.core.entities.interaction import Interaction
+
 
 class Recipe:
     ingredients: list[Ingredient]
+    interactions: list[Interaction]
+
+    def get_expected_period(self) -> tuple[int, int]:
+        season_ranges: list[tuple[int, int]] = [
+            (354, 365),
+            (1, 78),  # Winter: 21 December - 31 December & 1 January - 20 March
+            (79, 170),  # Spring: 21 March - 20 June
+            (171, 262),  # Summer: 21 June - 20 September
+            (263, 353),  # Autumn: 21 September - 20 December
+        ]
+
+        average_interaction_period = self.get_average_interaction_period()
+
+        for season_range in season_ranges:
+            if season_range[0] <= average_interaction_period <= season_range[1]:
+                return season_range
+
+        raise Exception(
+            f"Unexpected average interaction period: {average_interaction_period}"
+        )
+
+    def get_average_interaction_period(self) -> float:
+        average_interaction_period = 0
+        for interaction in self.interactions:
+            interaction_year_datetime = datetime(interaction.date.year, 1, 1)
+            average_interaction_period += (
+                interaction.date - interaction_year_datetime
+            ).days
+        average_interaction_period /= len(self.interactions)
+
+        return average_interaction_period
