13bd0f8ac7b6d657da614f558c8565a5a8cbb655
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-26 15:42:19 +0200

    Add data loader

---
+++ data/qeech_data/load_data.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
+
+
+def load_data():
+    download_dataset("shuyangli94/food-com-recipes-and-user-interactions")
+
+
+def download_dataset(dataset_name: str):
+    import kaggle
+
+    kaggle.api.authenticate()
+
+    kaggle.api.dataset_download_files(
+        dataset_name,
+        path=os.path.join(os.path.dirname(__file__), "..", "data"),
+        unzip=True,
+    )
+
+
+if __name__ == "__main__":
+    load_data()
