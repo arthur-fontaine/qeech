70219a46cfa443ee8d8132082da1d2e8643afa81
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-26 16:37:04 +0200

    First implementation of diet_score

--- data/qeech_data/application/services/diet_score.py
+++ data/qeech_data/application/services/diet_score.py
@@ -1,3 +1,12 @@
-def diet_score():
-    # TODO: implement
-    pass
+from qeech_data.core.entities.ingredient import Ingredient
+from qeech_data.core.entities.recipe import Recipe
+
+def diet_score(recipe: Recipe, forbidden_ingredients: list[Ingredient]):
+    recipe_ingredients = recipe.ingredients
+
+    for recipe_ingredient in recipe_ingredients:
+        for forbidden_ingredient in forbidden_ingredients:
+            if recipe_ingredient.id == forbidden_ingredient.id:
+                return 0
+            
+    return 1
