91b3f9ade112c5f7cb2dccb2ad29b97d81df6e45
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-27 14:39:00 +0200

    Fix unhashable objects when creating the graph

--- data/qeech_data/application/services/create_recipes_graph.py
+++ data/qeech_data/application/services/create_recipes_graph.py
@@ -12,11 +12,11 @@ if TYPE_CHECKING:
 def create_recipes_graph(*, recipes: list[Recipe], users: list[User]) -> nx.Graph:
     graph = nx.Graph()
 
-    graph.add_nodes_from(users, label="user")
-    graph.add_nodes_from(recipes, label="recipe")
+    graph.add_nodes_from(map(lambda x: x.id, users), label="user")
+    graph.add_nodes_from(map(lambda x: x.id, recipes), label="recipe")
 
     for user in users:
         for interaction in user.interactions:
-            graph.add_edge(user, interaction.recipe, label="interaction")
+            graph.add_edge(user.id, interaction.recipe.id, label="interaction")
 
     return graph
--- data/qeech_data/application/services/habits_score.py
+++ data/qeech_data/application/services/habits_score.py
@@ -11,7 +11,7 @@ if TYPE_CHECKING:
 
 def habits_score(recipe: Recipe, *, user: User, recipes_graph: nx.Graph) -> float:
     try:
-        shortest_path_length = nx.shortest_path_length(recipes_graph, source=user, target=recipe)
+        shortest_path_length = nx.shortest_path_length(recipes_graph, source=user.id, target=recipe.id)
         score = 1 / (1 + shortest_path_length)
         return score
     except nx.NetworkXNoPath:
