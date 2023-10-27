b568de84cc8b85d6ba92982be2a3710676fc7c68
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-10-27 14:41:41 +0200

    Implement recipe recommendation command

--- data/qeech_data/adapters/cli/commands/recommend_recipes.py
+++ data/qeech_data/adapters/cli/commands/recommend_recipes.py
@@ -1,4 +1,17 @@
+import sys
+from datetime import datetime
+
 from qeech_data.application.usecases.recommend_recipes import recommend_recipes as _recommend_recipes
+from qeech_data.core.repositories.user import UserRepository
 
 def recommend_recipes():
-    _recommend_recipes()
+    username = sys.argv[sys.argv.index("--username") + 1]
+    password = sys.argv[sys.argv.index("--password") + 1]
+
+    user = UserRepository.login(username, password)
+
+    date = datetime.now()
+    if "--date" in sys.argv:
+        date = datetime.fromisoformat(sys.argv[sys.argv.index("--date") + 1])
+
+    print(_recommend_recipes(user, date))
